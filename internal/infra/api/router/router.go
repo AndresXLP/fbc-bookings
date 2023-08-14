44cd2ca954afd418ce65b4e1c17e4416a4969c8c
Andres Puello <apuello1025@gmail.com>
2023-08-14 15:51:34 -0500

    fix: fix handle not found and logger router

--- internal/infra/api/router/router.go
+++ internal/infra/api/router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"net/http"
+
+	"fbc-bookings/internal/domain/entity"
 	"fbc-bookings/internal/infra/api/handler"
 	"fbc-bookings/internal/infra/api/router/groups"
 	"github.com/labstack/echo/v4/middleware"
@@ -23,8 +26,18 @@ func New(server *echo.Echo, bookings groups.Bookings) *Router {
 func (r *Router) Init() {
 	r.server.Use(middleware.CORS())
 	r.server.Use(middleware.Recover())
+	r.server.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, latency=${latency_human} status=${status}\n",
+	}))
+
 	basePath := r.server.Group("/api") //customize your basePath
 	basePath.GET("/health", handler.HealthCheck)
 
 	r.bookings.Resource(basePath)
+
+	r.server.RouteNotFound("*", func(c echo.Context) error {
+		return c.JSON(http.StatusNotFound, entity.Response{
+			Message: "Sorry, page not found",
+		})
+	})
 }
